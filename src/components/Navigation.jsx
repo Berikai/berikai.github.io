1b1cad423e7c1602a1f44b5a67856a035113229f
Berikai <berkayeren2009@gmail.com>
2025-02-04 16:54:43 +0300

    Add blog redirection

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -7,14 +7,15 @@ import { FontAwesomeIcon } from '@aduh95/preact-fontawesome'
 import { faArrowUpRightFromSquare } from '@fortawesome/free-solid-svg-icons'
 
 export default function Navbar() {
+    //showModal() shows in maintenance message
     return (
         <nav className="navbar">
             <div className="navbar-half1">
                 <NavButton id={"about-nav"} text={"About"} rotation={"rl"} action={() => AboutPage()}/>
-                <NavButton text={"Blog"} rotation={"rl"} action={() => showModal()}/>
+                <NavButton link text={(<><FontAwesomeIcon className="icon-nav-rl" size='xs' icon={faArrowUpRightFromSquare}/><>Blog</></>)} rotation={"rl"} href={"https://blog.berikai.dev"}/>
             </div>
             <div className="navbar-half2">
-                <NavButton link text={(<><FontAwesomeIcon className="icon-nav" size='xs' icon={faArrowUpRightFromSquare}/><>Console</></>)} rotation={"lr"} href={"https://console.berikai.dev"}/>
+                <NavButton link text={(<><FontAwesomeIcon className="icon-nav-lr" size='xs' icon={faArrowUpRightFromSquare}/><>Console</></>)} rotation={"lr"} href={"https://console.berikai.dev"}/>
                 <NavButton id={"portfolio-nav"} text={"Portfolio"} rotation={"lr"} action={() => PortfolioPage()}/>
             </div>
         </nav>
