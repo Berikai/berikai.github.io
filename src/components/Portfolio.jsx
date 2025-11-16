153ea562a453c47a93ffb596945516e163ab9dcf
Berikai <berkayeren2009@gmail.com>
2025-11-16 16:10:49 +0300

    Update portfolio

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -4,6 +4,11 @@ import portfolioBTE from '../assets/img/portfolio_bte2.png';
 import portfolioBTEW from '../assets/img/portfolio_btew.png';
 import portfolioPixbud from '../assets/img/portfolio_pixbud.gif';
 import portfolioOasis from '../assets/img/portfolio_oasis.png';
+import portfolioIxt from '../assets/img/portfolio_ixt.png';
+import portfolioAudiculator from '../assets/img/portfolio_audiculator.gif';
+
+import { FontAwesomeIcon } from '@aduh95/preact-fontawesome';
+import { faHtml5, faJs, faCss, faJava, faNodeJs } from '@fortawesome/free-brands-svg-icons';
 
 export default function Portfolio() {
     return (
@@ -14,54 +19,74 @@ export default function Portfolio() {
             </p>
             <div className="portfolio">
                 <div className="portfolio-element">
-                    <a className="portfolio-element-overlay" target="_blank" rel="noreferrer"
-                    href="https://github.com/Berikai/bitwig-theme-editor"></a>
-                    <img className="portfolio-img" width={68} src={portfolioBTE} alt="Bitwig Theme Editor"></img>
-                    <div className="portfolio-element-content dark">
-                        <div className="portfolio-element-title dark">Bitwig Theme Editor</div>
-                        <div className="portfolio-element-info dark">A patcher for Bitwig Studio, let's you customize the appearance of the most modular DAW in the world!</div>
+                    <div className='portfolio-element-inner'>
+                        <a className="portfolio-element-overlay" target="_blank" rel="noreferrer"
+                        href="https://ixt.to"></a>
+                        <img className="portfolio-img" width={88} src={portfolioIxt} alt="ixt.to"></img>
+                        <div className="portfolio-element-content dark">
+                            <div className="portfolio-element-title dark">ixt</div>
+                            <div className="portfolio-element-info dark">Stop emailing yourself notes. A fast, simple way to share text in real-time. No login required, ever.</div>
+                            <div className="portfolio-element-info framework dark">
+                                <div className="portfolio-element-info">Built with:</div>
+                                <FontAwesomeIcon icon={faNodeJs} style={{color: "#14c63b",}} />
+                                <FontAwesomeIcon icon={faHtml5} style={{color: "#ff9200",}} />
+                                <FontAwesomeIcon icon={faCss} style={{color: "#74C0FC",}} />
+                                <FontAwesomeIcon icon={faJs} style={{color: "#FFD43B",}} />
+                            </div>
+                        </div>
                     </div>
                 </div>
                 <div className="portfolio-element">
-                    <a className="portfolio-element-overlay" target="_blank" rel="noreferrer"
-                    href="https://bitwig.berikai.dev"></a>
-                    <div className="portfolio-element-content dark">
-                        <div className="portfolio-element-title dark">Bitwig Theme Editor WebUI</div>
-                        <div className="portfolio-element-info dark">Interactive visual playground to create and edit themes with ease and joy for Bitwig Studio</div>
-                    </div>
-                    <img className="portfolio-img" width={68} src={portfolioBTEW} alt="Bitwig Theme Editor WebUI"></img>
-                </div>
-                <div className="portfolio-element">
-                    <a className="portfolio-element-overlay" target="_blank" rel="noreferrer"
-                    href="https://github.com/Berikai/data/tree/main/pixbud"
-                    onClick={(e) => {
-                        e.preventDefault();
-                         if (window.confirm('This project is not finished and as of now it\'s closed source. You\'ll be redirected to a github page to download the Windows binaries of its prototype.\n\nWould you like to continue?')) {
-                            window.open(e.target.href, '_blank');
-                        }
-                    }}></a>
-                    <img className="portfolio-img" width={68} src={portfolioPixbud} alt="Pixbud"></img>
-                    <div className="portfolio-element-content dark">
-                        <div className="portfolio-element-title dark">Pixbud</div>
-                        <div className="portfolio-element-info dark">Your desktop buddy, loves to hang around on the rooftops of your application windows</div>
+                    <div className='portfolio-element-inner'>
+                        <a className="portfolio-element-overlay" target="_blank" rel="noreferrer"
+                        href="https://github.com/Berikai/bitwig-theme-editor"></a>
+                        <img className="portfolio-img" width={88} src={portfolioBTE} alt="Bitwig Theme Editor"></img>
+                        <div className="portfolio-element-content dark">
+                            <div className="portfolio-element-title dark">Bitwig Theme Editor</div>
+                            <div className="portfolio-element-info dark">A patcher for Bitwig Studio, customize the appearance of the most modular DAW in the world!</div>
+                            <div className="portfolio-element-info framework dark">
+                                <div className="portfolio-element-info">Built with:</div>
+                                <FontAwesomeIcon icon={faJava} style={{color: "#ff2600",}} />
+                            </div>
+                        </div>
                     </div>
                 </div>
                 <div className="portfolio-element">
-                    <a className="portfolio-element-overlay" target="_blank" rel="noreferrer"
-                    href="https://github.com/berikai/oasis-android"></a>
-                    <div className="portfolio-element-content dark">
-                        <div className="portfolio-element-title dark">OASIS Android</div>
-                        <div className="portfolio-element-info dark">An unofficial mobile application for the student information system of Izmir University of Economics</div>
+                    <div className='portfolio-element-inner'>
+                        <a className="portfolio-element-overlay" target="_blank" rel="noreferrer"
+                        href="https://audiculator.berikai.dev"></a>
+                        <img className="portfolio-img" width={88} src={portfolioAudiculator} alt="Bitwig Theme Editor WebUI"></img>
+                        <div className="portfolio-element-content dark">
+                            <div className="portfolio-element-title dark">Audiculator</div>
+                            <div className="portfolio-element-info dark">A web-based real-time 3D audio visualizer with rich features, powered by three.js </div>
+                            <div className="portfolio-element-info framework dark">
+                                <div className="portfolio-element-info">Built with:</div>
+                                <img src='https://upload.wikimedia.org/wikipedia/commons/3/3f/Three.js_Icon.svg' width={12} height={12} alt='ThreeJS Logo' style={{opacity: 0.7}}/>
+                                <FontAwesomeIcon icon={faJs} style={{color: "#FFD43B",}} />
+                            </div>
+                        </div>
                     </div>
-                    <img className="portfolio-img" width={68} src={portfolioOasis} alt="OASIS Android"></img>
                 </div>
                 <div className="portfolio-element">
-                    <a className="portfolio-element-overlay" target="_blank" rel="noreferrer"
-                    href="https://github.com/berikai/wr2-server-connector"></a>
-                    <img className="portfolio-img" width={68} src="https://user-images.githubusercontent.com/18515671/160943154-ebd29b37-19ec-4687-899a-24d555c46e7b.png"></img>
-                    <div className="portfolio-element-content dark">
-                        <div className="portfolio-element-title dark">WR2 Server Connector</div>
-                        <div className="portfolio-element-info dark">A simple tool to play World Racing 2 online, since the official multiplayer tool is deprecated</div>
+                    <div className='portfolio-element-inner'>
+                        <a className="portfolio-element-overlay" target="_blank" rel="noreferrer"
+                        href="https://github.com/Berikai/data/tree/main/pixbud"
+                        onClick={(e) => {
+                            e.preventDefault();
+                            if (window.confirm('This project is not finished and as of now it\'s closed source. You\'ll be redirected to a github page to download the Windows binaries of its prototype.\n\nWould you like to continue?')) {
+                                window.open(e.target.href, '_blank');
+                            }
+                        }}></a>
+                        <img className="portfolio-img" width={88} src={portfolioPixbud} alt="Pixbud"></img>
+                        <div className="portfolio-element-content dark">
+                            <div className="portfolio-element-title dark">Pixbud</div>
+                            <div className="portfolio-element-info dark">Your desktop buddy, loves to hang around on the rooftops of your application windows</div>
+                            <div className="portfolio-element-info framework dark">
+                                <div className="portfolio-element-info">Built with:</div>
+                                <img src='https://upload.wikimedia.org/wikipedia/commons/b/bd/Logo_C_sharp.svg' width={12} height={12} alt='.NET Logo' style={{opacity: 0.7}}/>
+                                <img src='https://upload.wikimedia.org/wikipedia/commons/7/7d/Microsoft_.NET_logo.svg' width={12} height={12} alt='WinAPI Logo' style={{opacity: 0.7}}/>
+                            </div>
+                        </div>
                     </div>
                 </div>
                 <div className="text-about" style="margin-top: -4px;">and many more, check my <a href="https://github.com/berikai" target="_blank" rel="noreferrer">Github</a>!</div>
